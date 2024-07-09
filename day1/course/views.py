5b8c84001b620a630b7b1601b28023b4b1af4350
Taukir_Sumer <Shahinsheikh2931977@gmail.com>
2024-07-09 12:24:08 +0530

    Day1 learning

---
+++ day1/course/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# First View
+
+def learn_django(request):
+    # return HttpResponse('Hello World')
+    return HttpResponse('<h1>Hello World</h1>')
+
+# Multiple View
+
+def learn_math(request):
+    a=20+20
+    return HttpResponse(a)
+
+def learn_format(request):
+    a="Taukir"
+    return HttpResponse(f'hello {a}')
+
+def home(request):
+    return HttpResponse('Home')
